a6a7dc3d290d67ef648da87fa99d9abdd94fb34a
Eric Theise <erictheise@gmail.com>
2026-01-23 20:22:50 -0800

    Addresses https://github.com/OpenHistoricalMap/issues/issues/1266 by extending the exceptRegex to allow "google" as part of a url path

--- modules/validations/incompatible_source.js
+++ modules/validations/incompatible_source.js
@@ -14,7 +14,7 @@ const incompatibleRules = [
   {
     id: 'google',
     regex: /(google)/i,
-    exceptRegex: /((books|drive)\.google|google\s?(books|drive|plus))|(esri\/Google_(Africa|Open)_Buildings)/i
+    exceptRegex: /((books|drive)\.google|google\s?(books|drive|plus))|(esri\/Google_(Africa|Open)_Buildings)|(:\/\/\S+\/\S+(google)\S+)/i
   }
 ];
 
